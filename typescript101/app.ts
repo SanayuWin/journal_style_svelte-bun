1f93ad031c7d18af9df750946ce0bf0cb0c82998
kittitnun <kittitnun@gmail.com>
2024-02-27 14:43:12 +0700

    up

---
+++ typescript101/app.ts
@@ -0,0 +1,10 @@
+// let str :number
+// str =10
+// str = 10
+// str = true
+// console.log(typeof str)
+const sum = (a:number,b:number)=>{
+    return a + b 
+}
+const total =sum(5,10)
+console.log(total)
